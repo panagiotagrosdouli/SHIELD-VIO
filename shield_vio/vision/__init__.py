7f5a1915e349e967957ff673d90dd3afe91edc79
Panagiota Grosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-07-30 22:26:41 +0300

    chore: expose visual geometry primitives

---
+++ shield_vio/vision/__init__.py
@@ -0,0 +1,5 @@
+"""Visual front-end components for SHIELD-VIO."""
+
+from shield_vio.vision.two_view_geometry import RelativePoseMeasurement, estimate_relative_pose
+
+__all__ = ["RelativePoseMeasurement", "estimate_relative_pose"]
